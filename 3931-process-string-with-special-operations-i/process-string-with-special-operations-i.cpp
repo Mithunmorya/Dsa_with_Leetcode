def16ac2573933f26badc7edb53803a1fea88ccf
Mithun Kumar <mithunkumar181204@gmail.com>
2026-06-17 00:08:43 +0530

    Time: 4 ms (63.64%) | Memory: 39.7 MB (69.70%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -1,34 +1,19 @@
 class Solution {
 public:
 
-    string rever(string r){
-        if(r.size()==0) return "";
-        string ans;
-        for(int i = r.size()-1;i>=0;i--){
-            ans.push_back(r[i]);
-        }
-        return ans;
-    }
-    string remove(string r){
-        if(r.size()==0) return "";
-        string ans;
-        for(int i=0;i<r.size()-1;i++){
-            ans.push_back(r[i]);
-        }
-        return ans;
-    }
     string processStr(string s) {
         if(s.size()==0) return "";
         string ans;
         for(int i = 0;i<s.size();i++){
             if(s[i]=='*'){
-                ans = remove(ans);
+                if(!ans.empty())
+                ans.pop_back();
             }
             else if(s[i]=='#')
             ans = ans+ans;
 
             else if(s[i]=='%')
-            ans = rever(ans);
+            reverse(ans.begin(),ans.end());
 
             else
             ans.push_back(s[i]);
