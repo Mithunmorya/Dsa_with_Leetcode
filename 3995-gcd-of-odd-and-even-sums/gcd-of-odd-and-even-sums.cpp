5b61692c9f07a43da72f562aed19dbe4ef5eafc0
Mithun Kumar <mithunkumar181204@gmail.com>
2026-07-16 16:59:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (85.03%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        long long sumodd = 0;
-        long long sumeven = 0;
-        int j= 1;
-        int k = 2;
-        for(int i = 0;i<n;i++){
-            sumodd += j;
-             j += 2;
-        }
-        for(int i = 0;i<n;i++){
-            sumeven += k;
-            k += 2;
-            
-        }
-        // int smallest = sumodd;
-        // if(sumodd>sumeven)
-        // smallest = sumeven;
-
-        // for(int i = smallest;i>=0;i--){
-        //     if(sumodd%i==0 && sumeven%i==0)
-        //     return i;
+        // long long sumodd = 0;
+        // long long sumeven = 0;
+        // int j= 1;
+        // int k = 2;
+        // for(int i = 0;i<n;i++){
+        //     sumodd += j;
+        //      j += 2;
         // }
-        // return -1;
-        return __gcd(sumodd,sumeven);
+        // for(int i = 0;i<n;i++){
+        //     sumeven += k;
+        //     k += 2;
+            
+        // }
+        // // int smallest = sumodd;
+        // // if(sumodd>sumeven)
+        // // smallest = sumeven;
+
+        // // for(int i = smallest;i>=0;i--){
+        // //     if(sumodd%i==0 && sumeven%i==0)
+        // //     return i;
+        // // }
+        // // return -1;
+        // return __gcd(sumodd,sumeven);
+        long long sumodd = n*n;
+        long long sumeven = (n*n)+n;
+        return gcd(sumodd,sumeven);
     }
 };
